2e7fbd1a7fba482964bc0af48cffca3963c27ad5
Jason <Jasondelfyette@gmail.com>
2020-03-03 16:54:54 -0500

    Info Plist changed

--- WeatherAppII/SecretKeys/SecretKeys.swift
+++ WeatherAppII/SecretKeys/SecretKeys.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 struct SecretKeys {
-  static let apiKey = "a"
-  static let accessID = "a"
+  static let apiKey = "QI9PSvSyOTWsXuUE2yprWjruM6jA3u8GxZycqrVa"
+  static let accessID = "4u5xzN4BsqrY333a2SfCJ"
 }
